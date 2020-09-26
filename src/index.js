20962b7db9bf6f13a2adf309b3e0c3194a909059
Jem <chrisvin1997@gmail.com>
2020-09-27 04:48:51 +0530

    Comment steps for the command

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ const {Command, flags} = require('@oclif/command')
 class EasyAndroidEmulatorCommand extends Command {
   async run() {
     const {flags} = this.parse(EasyAndroidEmulatorCommand)
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from .\\src\\index.js`)
+    const name = flags.name || 'Pixel 3 XL'
+
+    // STEPS:
+    // 1. Parse CSV to get specs for the model (will need to check based on both device name & model)
+    // 2. Once match spec is found, Check if suitable emulator is already available,
+    // 3. If so, directly start the emulator (skip steps 4, 5 & 7)
+    //    (NOTE: Since the emulator already exists, it was persisted, do NOT delete it)
+    // 4. If not, then download required SDKs using `sdkmanager` (if they're not already there).
+    // 5. Once SDKs are ready, create the emulator using `avdmanager`.
+    // 6. Start the emulator using `emulator`.
+    // 7. Unless the user has choosen to persist the avd, delete the avd using `avdmanager`
+
   }
 }
 
